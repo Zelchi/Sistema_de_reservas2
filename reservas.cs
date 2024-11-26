345453068ec7afe62b51b4ddfba912184a0d5aec
Davi Lima <tininhakawaii@gmail.com>
2024-11-26 17:58:38 -0300

    Add project files.

---
+++ reservas.cs
@@ -0,0 +1,15 @@
+﻿class Reservas
+{
+    public string nome { get; set; }
+    public string cpf { get; set; }
+    public int diaReserva{ get; set; }
+    public int quantidadePessoas { get; set; }
+
+    public Reservas(string nome, string cpf, int diaReserva, int quantidadePessoas)
+    {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.diaReserva = diaReserva;
+        this.quantidadePessoas = quantidadePessoas;
+    }
+}
